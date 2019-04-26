dd41bf828f900ec8d9acb06f74e5bbddd35d67b5
valdmel <valmneves92@gmail.com>
2019-04-25 21:54:21 -0300

    - [Update]: A few small changes in the project;

--- Projeto/src/Main.java
+++ Projeto/src/Main.java
@@ -1,37 +1,6 @@
-import br.mackenzie.fci.ec.lp2.dao.AutorDAO;
-import br.mackenzie.fci.ec.lp2.dao.LivroDAO;
-import br.mackenzie.fci.ec.lp2.dao.UsuarioDAO;
-import br.mackenzie.fci.ec.lp2.model.Autor;
-import br.mackenzie.fci.ec.lp2.model.Livro;
-import br.mackenzie.fci.ec.lp2.model.Usuario;
-
 public class Main
 {
     public static void main(String[] args) throws Exception
-    { 
-//        //Área para testes
-//
-//        Autor autor = new Autor();
-//
-//        autor.setID(2);
-//
-//        AutorDAO autorDAO = new AutorDAO();
-//
-//
-//        Livro livro = new Livro();
-//
-//        LivroDAO livroDAO = new LivroDAO();
-//
-//        livro.setID(1);
-//
-//        livroDAO.remover(livro);
-//
-//        //NÃO MEXER
-//        System.out.println("ID_LIVRO: "+livro.getID());
-//        System.out.println("ID_AUTOR: "+livroDAO.listarPorID(livro).getIDAutor().getID());
-//        System.out.println("NOME_AUTOR: "+livroDAO.listarPorID(livro).getIDAutor().getNome());
-//        System.out.println("NOME_LIVRO: "+livroDAO.listarPorID(livro).getNome());
-//
-//        livroDAO.remover(livro);
+    {
     }
 }
